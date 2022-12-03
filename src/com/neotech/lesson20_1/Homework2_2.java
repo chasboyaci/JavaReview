49b8dfbae110e43f8a611c6176ae764cffc2e40d
chasboyaci <catherine.hasboyaci@gmail.com>
2022-12-03 11:59:13 -0500

    new update

--- src/com/neotech/lesson20_1/Homework2_2.java
+++ src/com/neotech/lesson20_1/Homework2_2.java
@@ -5,6 +5,7 @@ import com.neotech.lesson20.Homework2;
 public class Homework2_2 {
 
 	public static void main(String[] args) {
+		//Homework
 		//Homework2 st1 = new Homework2();
 		Homework2 st2 = new Homework2("Catherine");
 		//Homework2 st3 = new Homework2("Catherine", "Hasboyaci");
