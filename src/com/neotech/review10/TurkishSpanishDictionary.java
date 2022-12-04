1f2308caaba751226024904fe004ad681f966a35
chasboyaci <catherine.hasboyaci@gmail.com>
2022-12-04 11:30:57 -0500

    notes

--- src/com/neotech/review10/TurkishSpanishDictionary.java
+++ src/com/neotech/review10/TurkishSpanishDictionary.java
@@ -1,9 +1,28 @@
 package com.neotech.review10;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class TurkishSpanishDictionary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Map<String, String> dictionary = new TreeMap<>();
+		dictionary.put("ev", "casa");
+		dictionary.put("kedi", "gato");
+		dictionary.put("esek", "burro");
+		dictionary.put("bayrak", "bandera");
+		dictionary.put("barsaf", "bandera");
+		dictionary.put("carsaf", "bandera");
+		
+		System.out.println(dictionary);
+		
+		//Set -> No duplicates
+		//List -> Insertion order is perserved
+		Set<String> turkishWords = dictionary.keySet();
+		
+		System.out.println("keys -> " + turkishWords);
 
 	}
 
